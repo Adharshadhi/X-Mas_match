c0fa0c13fc03ffd89b7b23decee4fd064de1f04e
Adharsh <68592390+Adharshadhi@users.noreply.github.com>
2021-12-21 21:48:45 +0530

    basic

--- JS/script.js
+++
@@ -1,7 +0,0 @@
-const matchBtn = document.querySelector("#match_btn");
-
-matchBtn.addEventListener("click", () => {
-  let student_name = $("#student_name").val();
-  let student_num = $("#phone_num").val();
-  let student_year = $("#year").val();
-});
